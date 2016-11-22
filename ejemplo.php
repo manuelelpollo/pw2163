1182e0418ceca398df5f1a39e133898eaeb12bac
manuelelpollo <manuel_pollo95@hotmail.com>
2016-11-22 12:03:34 -0700

    php2

--- ejemplo.php
+++ ejemplo.php
@@ -1,3 +1,67 @@
+<?php
+		//preguntar si los valores existen
+		$oculto=""; //inicializando la variable
+		$usuario="";
+		$nombre="";
+		$clave="";
+		$tipo="";
+		if(isset($_POST["txtOculto"])) //si trae valores
+		{
+			$oculto=$_POST["txtOculto"];
+		}
+		if(isset($_POST["txtUsuario"])) //si trae valores
+		{
+			$usuario=$_POST["txtUsuario"];
+		}
+		if(isset($_POST["txtNombre"])) //si trae valores
+		{
+			$nombre=$_POST["txtNombre"];
+		}
+		if(isset($_POST["txtClave"])) //si trae valores
+		{
+			$clave=$_POST["txtClave"];
+		}
+		if(isset($_POST["txtTipo"])) //si trae valores
+		{
+			$tipo=$_POST["txtTipo"];
+		}
+
+		function guardaUsuario($usuario,$nombre,$clave,$tipo)
+		{
+			//conectarse al servidor MySQL.
+			//MySQL (servidor, usuario,contraseña)
+			$conexion= mysql_connect("localhost","root","");
+			//seleccionamos la bd
+			mysql_select_db("bd2163");
+			$consulta="insert into usuarios values('".$usuario."','".$nombre."','".$clave."','".$tipo."')";
+			//ejecutamos la consulta
+			mysql_query($consulta);
+			//preguntamos si hubo insercion
+			if(mysql_affected_rows()>0)
+			{
+				print "registro guardado";
+			}
+			else
+			{
+				print "no se pudo guardar el registro ";
+			}
+
+		}
+
+
+		switch ($oculto) {
+			case 'guardaUsuario':
+				guardaUsuario($usuario,$nombre,$clave,$tipo);
+				break;
+			
+			default:
+				# code...
+				break;
+		}
+
+
+
+?>
 <h1>ALTA DE USUARIOS</h1>
 <form action="ejemplo.php" method="POST">
 	<input type="hidden" name="txtOculto" value="guardaUsuario">
@@ -15,10 +79,6 @@
 	<br>
 	<input type="submit" value="Enviar">
 </form>
-<?php
 
-
-
-?>
 
 
